9ae36854cc4622fef68b68d38d2cc4a2341c8479
Roki_Crazy <89066095+roki200@users.noreply.github.com>
2022-04-26 18:32:09 +0700

    Update echo.bot.py

--- echo.bot.py
+++ echo.bot.py
@@ -17,21 +17,11 @@ async def process_start_command(message: types.Message):
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне сообщение и  отправлю тебе в ответ!"
-
-
-@dp.message_handler(commands=['owner'])
-async def process_help_command(message: types.Message):
-    await message.reply("Мой создатель👨‍💻 @roki_crayzy,15 y.o , 🇷🇺/🇹🇯")
                  
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
-
-
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_chat.id, msg.text)
 
 
 if __name__ == '__main__':
